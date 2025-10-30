3cb20f4a5c8d3eb5087df61416747b919c8bd645
HalleyWorkspace <linkhalley@comcast.net>
2025-10-29 20:29:58 -0400

    created text_nod_to_html_node function

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,5 @@
 from enum import Enum
-
+from htmlnode import LeafNode
 class TextType(Enum):
     PLAIN = "plain"
     BOLD = "bold"
@@ -27,3 +27,18 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.PLAIN:
+            return LeafNode(value=text_node.text)
+        case TextType.BOLD:
+            return LeafNode(tag="b",value=text_node.text)
+        case TextType.ITALIC:
+            return LeafNode(tag="i",value=text_node.text)
+        case TextType.CODE:
+            return LeafNode(tag="code",value=text_node.text)
+        case TextType.LINK:
+            return LeafNode(tag="a",value=text_node.text,href=text_node.url)
+        case TextType.IMAGE:
+            return LeafNode(tag="img",value="",src=text_node.url,alt=text_node.text)
